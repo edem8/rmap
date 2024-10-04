45ec4ea151736ac310abe53daf04e2fc50ee9974
edem8 <eddieblack159@gmail.com>
2024-10-04 10:23:04 +0000

    chore: mini-documentting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    
+      {/* 
+
+        REACT ROADMAP
+
+        This repo holds a series of branches
+        which are essentially concepts in react that i have chanced 
+        on, explored or found interesting
+
+        I will probably make an officail blog on too
+
+        CHECKING OUT A SPECIFIC CONCEPT
+        - All branches are named after a concept
+        - There is at least one use case implementation for every concept
+        - Go to specific branch to see concept and implementation
+      */}
     </div>
   );
 }
